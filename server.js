4c1c193e87842d794d56892ed2311a9762643b86
Gaurav kumar <gaurav@kreyaconsulting.com>
2024-04-04 14:50:44 +0530

    Authentications and authorizations using bcrypt password

--- server.js
+++ server.js
@@ -1,29 +1,34 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
-
 require('dotenv').config();
+
+const passport = require('./auth');
 const bodyParser = require("body-parser");
+
 app.use(bodyParser.json());
 const PORT = process.env.PORT || 3000;
 
 
 
-//middleware functions
-
+//middleware functions for the logging functionalities fg
 const logRequest = (req,res,next)=>{
   console.log(`${new Date().toLocaleString()} request made to :  ${req.originalUrl}`);
   // console.log(`${new Date().toLocaleString()} request made to :  ${req.originalUrl}`);
   next();
 }
-
 app.use(logRequest);
 
-app.get("/", logRequest, function (req, res) {
+
+
+
+
+app.use(passport.initialize());
+const localAuthMiddleware =  passport.authenticate('local',{session:false});
+
+app.get("/",localAuthMiddleware, function (req, res) {
   res.send("welcome to hotel");
 });
-
-
 
 
 
@@ -31,7 +36,7 @@ app.get("/", logRequest, function (req, res) {
 const personRoutes = require('./routes/personRoute');
 const menuItemRoutes = require('./routes/menuItemRoutes');
 
-app.use('/person',personRoutes);
+app.use('/person',localAuthMiddleware,personRoutes);
 app.use('/menu',menuItemRoutes);
 
 
